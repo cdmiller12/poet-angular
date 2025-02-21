411a0099fc901b29a22aa90996f08d32a4853e27
cdmiller12 <Coll.d.miller@gmail.com>
2025-02-21 16:58:47 +0000

    Removing headers, not needed

--- projects/app/src/app/poetry-service.service.ts
+++ projects/app/src/app/poetry-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -10,23 +10,17 @@ export class PoetryServiceService {
 
   constructor(private http: HttpClient) {}
 
-  public corsHeaders = new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': 'https://turbo-space-doodle-jj7g95wp9x96cpp6v-4300.app.github.dev/*',
-    'Access-Control-Request-Headers': '*'
-  });
   
     getPoets(): Observable<any>{
-      return this.http.get<any>('https://poetrydb.org/author', {headers: this.corsHeaders}).pipe(catchError(this.erroHandler));
+      return this.http.get<any>('https://poetrydb.org/author').pipe(catchError(this.erroHandler));
     }
 
     getAuthor(name: string): Observable<any>{
-      return this.http.get<any>('https://poetrydb.org/author/'+name+"/title", {headers: this.corsHeaders}).pipe(catchError(this.erroHandler));
+      return this.http.get<any>('https://poetrydb.org/author/'+name+"/title").pipe(catchError(this.erroHandler));
     }
 
     getPoemByAuthor(authorName: string, poemName: string): Observable<any>{
-      return this.http.get<any>('https://poetrydb.org/author,title/'+authorName+";"+poemName, {headers: this.corsHeaders}).pipe(catchError(this.erroHandler));
+      return this.http.get<any>('https://poetrydb.org/author,title/'+authorName+";"+poemName).pipe(catchError(this.erroHandler));
     }
 
     erroHandler(error: HttpErrorResponse) {
